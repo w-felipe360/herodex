f97fa64775b9d568d8f62954f5a96a06d17d8c0a
willian <w.felipebraz@gmail.com>
2024-08-30 04:24:15 -0300

    🔨 chore: Add API service module

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import md5 from "md5";
+
+const baseUrl = 'http://gateway.marvel.com/v1/public';
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY!;
+const ts = Number(new Date());
+const hash = md5(ts + privateKey + publicKey);
+
+const api = axios.create({
+    baseURL: baseUrl,
+    params: {
+        ts,
+        apikey: publicKey,
+        hash,
+    }
+});
+
+export default api;
