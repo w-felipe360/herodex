1f2ab3afb26f467b783ca6d420a136d5234e2e38
willian <w.felipebraz@gmail.com>
2024-08-30 04:51:02 -0300

    Update Marvel API base URL to use HTTPS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import md5 from "md5";
 
-const baseUrl = 'http://gateway.marvel.com/v1/public';
+const baseUrl = 'https://gateway.marvel.com/v1/public';
 const publicKey = process.env.PUBLIC_KEY!;
 const privateKey = process.env.PRIVATE_KEY!;
 const ts = Number(new Date());
